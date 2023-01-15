0f58b2f5fbd0d1f6d20e3034d8707417095f5d90
Maksym Movchan <xon.dev@protonmail.com>
2023-01-15 13:18:30 +0000

    feat: Added docker

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,6 @@
+import os
 import logging
-from types import NoneType
 from flask import Flask
-from nodes.binary_file import BinaryFile
-from nodes.buffer_file import BufferFile
-from nodes.directory import Directory
-from nodes.log_text_file import LogTextFile
 from server.binary_file_server import binary_file_server
 from server.buffer_file_server import buffer_file_server
 from server.directory_server import directory_server
@@ -22,8 +18,10 @@ app.register_blueprint(log_text_file_server)
 @app.route("/reload", methods=["POST"])
 def reload():
     root.children = []
-    return 'Reloaded', 200
-    
+    return "Reloaded", 200
 
-if __name__ == '__main__':
-    app.run(port=3333, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
